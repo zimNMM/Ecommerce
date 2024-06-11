70b0abce794c266a4f0f962c10900576090a6429
zimNMM <zim_@live.co.uk>
2024-06-11 18:53:49 +0300

    added basic auth

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,5 +20,8 @@ from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
